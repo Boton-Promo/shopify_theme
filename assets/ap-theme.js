2abe0235da9468d9fd9947fdbffb2ef49746042e
slaternight <slayderreyescuellar@gmail.com>
2024-10-25 11:25:02 -0500

    the line 788y7 was modify the queriySelector to querySelectorAll for each div with the class

--- assets/ap-theme.js
+++ assets/ap-theme.js
@@ -7884,7 +7884,7 @@
         }
 
         // Nueva animación para drawer__header__contact
-      const headerContact = this.querySelector(".drawer__header__contact");
+      const headerContact = this.querySelectorAll(".drawer__header__contact");
       if (headerContact) {
         effects.push(
           new ApEffectOfCustomKeyframe(
