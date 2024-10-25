70482e7b828cce7e7380c8caaa722f2286ac0894
slaternight <slayderreyescuellar@gmail.com>
2024-10-25 10:49:49 -0500

    changes reverted

--- assets/ap-theme.js
+++ assets/ap-theme.js
@@ -7927,7 +7927,7 @@
             })
           )
         );
-        const bottomBar = this.querySelector(".drawer__footer, .drawer__header__contact");
+        const bottomBar = this.querySelector(".drawer__footer");
         if (bottomBar) {
           effects.push(
             new ApEffectOfCustomKeyframe(
