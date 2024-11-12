c43dec63958044c747dfde441c54c1ea7f6311c2
slaternight <slayderreyescuellar@gmail.com>
2024-11-12 11:56:04 -0500

    the toggle code was edit

--- assets/global.js
+++ assets/global.js
@@ -858,32 +858,31 @@ class VariantSelects extends HTMLElement {
   toggleAddButton(disable = true, text, modifyClass = true) {
     const productForm = document.getElementById(`product-form-${this.dataset.section}`);
     if (!productForm) return;
+  
     const addButton = productForm.querySelector('[name="add"]');
     const addButtonText = productForm.querySelector('[name="add"] > span');
-   
     const inventoryNote = document.querySelector('.inventoryNote');
-    const inventoryHtml = `We have ${variantStock[this.currentVariant.id]} in stock`;
-    
+  
     if (!addButton) return;
-
+  
     if (disable) {
-        addButton.setAttribute('disabled', 'disabled');
-        if (text) addButtonText.textContent = text;
-        inventoryNote.innerHTML = "";
+      addButton.setAttribute('disabled', 'disabled');
+      if (text) addButtonText.textContent = text;
+      inventoryNote.innerHTML = '';
     } else {        
-        addButton.removeAttribute('disabled');
-        addButtonText.textContent = window.variantStrings.addToCart;
-
-        // Verificar si el stock es mayor que 0
-        if (variantStock[this.currentVariant.id] > 0) {
-          inventoryNote.textContent = inventoryHtml;
-        } else {
-          inventoryNote.textContent = "";
-        }
+      addButton.removeAttribute('disabled');
+      addButtonText.textContent = window.variantStrings.addToCart;
+  
+      if (variantStock[this.currentVariant.id] > 0) {
+        inventoryNote.textContent = `We have ${variantStock[this.currentVariant.id]} in stock`;
+      } else {
+        inventoryNote.textContent = 'Out of stock';
+      }
     }
-
+  
     if (!modifyClass) return;
-}
+  }
+  
 
 
   setUnavailable() {
