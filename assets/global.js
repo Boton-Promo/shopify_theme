e0717521d02120a493d538245352da4acb27e043
slaternight <slayderreyescuellar@gmail.com>
2024-11-30 13:25:10 -0500

    the on to off

--- assets/global.js
+++ assets/global.js
@@ -692,7 +692,7 @@ class SlideshowComponent extends SliderComponent {
   }
 
   play() {
-    this.slider.setAttribute('aria-live', 'on');
+    this.slider.setAttribute('aria-live', 'off');
     clearInterval(this.autoplay);
     this.autoplay = setInterval(this.autoRotateSlides.bind(this), this.autoplaySpeed);
   }
